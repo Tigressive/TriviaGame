58d9f6aa9ccfa710fc0114272374c9ce3d79ee1a
Ashton Atkinson <rascalcrafters@gmail.com>
2022-04-15 16:04:31 -0600

    Added some functionality for the questions and answers.

--- trivia.js
+++ trivia.js
@@ -1,5 +1,6 @@
 let round = 0;
 let tries = 0;
+let score = 0;
 
 function roundUp() {
     round++;
@@ -12,28 +13,63 @@ function triesUp() {
 
 }
 
-function game(){
+const game = async () => {
+    //https://opentdb.com/api.php?amount=10&category=25&difficulty=easy&type=multiple
+    //https://opentdb.com/api_category.php
+    let categories = [];
+    const data = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')
+        .then((response) => {
+            console.log('response: ', response);
+            if (response.status !== 200) {
+                console.log('Something went wrong');
+            }
+            return response.json();
+        });
 
-    roundUp();
+    data.results.forEach((cat) =>{
+        const div = document.getElementById('div');
+        const pTag = document.createElement('p');
+        div.appendChild(pTag);
+        pTag.innerHTML = cat.category;
+        categories.push(cat.category);
+        console.log(data.results[0]);
+        // console.log(cat);
+    })
+
+
+     do {
+         let question = document.createElement('p')
+         question.innerHTML = `${data.results[0].question}`
+         let answerOneButton = document.createElement('input');
+         div.appendChild(question)
+         answerOneButton.type = 'radio'
+         answerOneButton.value = `${data.results[0].correct_answer}`
+         div.appendChild(answerOneButton);
+
+
+
+     }while (tries >= 3 || round >= 15)
+
 
 }
 
 
 
-const Menu = async () =>
-{
+const Menu = async () => {
+
     const container = document.createElement('div');
     container.className = 'menuContainer';
     const select = document.createElement('select');
     const data = await fetch('https://opentdb.com/api_category.php')
         .then((response) => {
             console.log('response: ', response);
-            if (response.status !== 200)
-            {
+            if (response.status !== 200) {
                 console.log('Something went wrong');
             }
             return response.json();
         });
+
+
     const waste = document.createElement('option');
     select.appendChild(waste);
     console.log('data: ', data);
@@ -46,4 +82,10 @@ const Menu = async () =>
     });
     container.appendChild(select);
     return container;
+}
+
+
+function scoreUp(round, score){
+    let calcScore = score * round;
+    return calcScore
 }
